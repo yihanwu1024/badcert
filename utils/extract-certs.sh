b2e506efb88454d6452c64cdeb1e989ae9f16439
yihanwu1024 <48046869+yihanwu1024@users.noreply.github.com>
2022-10-09 14:46:50 -0500

    Update extract-certs.sh

--- utils/extract-certs.sh
+++ utils/extract-certs.sh
@@ -5,12 +5,17 @@ CERTIFICATE_STORE_PATH=$2
 
 PAYLOAD_PATH=$1
 
-PAYLOAD_SHA256SUM=$( sha256sum ${PAYLOAD_PATH} | head -c 64 )
+PAYLOAD_SHA256SUM=$( sha256sum "${PAYLOAD_PATH}" | head -c 64 )
 
-# mkdir "${CERTIFICATE_STORE_PATH}"
-osslsigncode extract-signature -pem -in ${PAYLOAD_PATH} -out ${PAYLOAD_SHA256SUM}-all.pem > /dev/null
-LIST_ELEMENTS=$(openssl asn1parse -in ${PAYLOAD_SHA256SUM}-all.pem | grep 'pkcs7-signedData')
 IFS=$'\n'
+mkdir -p "${CERTIFICATE_STORE_PATH}"
+osslsigncode extract-signature -pem -in "${PAYLOAD_PATH}" -out ${PAYLOAD_SHA256SUM}-all.pem
+if [ $? == 255 ]
+then
+  exit 1
+fi
+LIST_ELEMENTS=$(openssl asn1parse -in ${PAYLOAD_SHA256SUM}-all.pem | grep 'pkcs7-signedData')
+
 for e in $LIST_ELEMENTS
 do
   OFFSET=$(( $( echo $e | sed 's/\:.*//' - ) - 4 )) # Wanted SEQUENCE always appears 4 bytes before OBJECT:pkcs7-signedData
@@ -21,17 +26,17 @@ do
     openssl asn1parse -in ${PAYLOAD_SHA256SUM}-all.pem -out ${PAYLOAD_SHA256SUM}-${OFFSET}.der -noout -offset +${OFFSET}
   fi
   openssl pkcs7 -inform DER -in ${PAYLOAD_SHA256SUM}-${OFFSET}.der -print_certs -out ${PAYLOAD_SHA256SUM}-${OFFSET}.pem
-  csplit -z -f ${PAYLOAD_SHA256SUM}-${OFFSET}- -b '%d.pem' ${PAYLOAD_SHA256SUM}-${OFFSET}.pem '%-----BEGIN CERTIFICATE-----%' '/-----BEGIN CERTIFICATE-----/' '{*}' > /dev/null
+  csplit -z -f ${PAYLOAD_SHA256SUM}-${OFFSET}- -b '%d.pem' ${PAYLOAD_SHA256SUM}-${OFFSET}.pem '%-----BEGIN CERTIFICATE-----%' '/-----BEGIN CERTIFICATE-----/' '{*}'
   rm ${PAYLOAD_SHA256SUM}-${OFFSET}.pem
   rm ${PAYLOAD_SHA256SUM}-${OFFSET}.der
   for f in ${PAYLOAD_SHA256SUM}-${OFFSET}-*
   do
-    # Optionally sanitize split files
     FINGERPRINT=$( openssl x509 -in $f -fingerprint -noout | sed -e 's/SHA1 Fingerprint=//' -e 's/\://g' - )
     SUBJECT_SHORTENED=$( openssl x509 -in $f -subject -nameopt utf8 -noout | sed -E -e 's/subject=//' -e 's/businessCategory=[0-9a-zA-Z ]+(, )?//' -e 's/serialNumber=[0-9a-zA-Z]+(, )?//' | head -c 255 )
     DATE_NOTBEFORE=$( openssl x509 -in $f -dates -dateopt iso_8601 -noout | grep 'notBefore=' | sed -e 's/notBefore=//' -e 's/ ..:..:..*//' - )
     DATE_NOTAFTER=$( openssl x509 -in $f -dates -dateopt iso_8601 -noout | grep 'notAfter=' | sed -e 's/notAfter=//' -e 's/ ..:..:..*//' - )
     mkdir -p "${CERTIFICATE_STORE_PATH}/${SUBJECT_SHORTENED}"
+    echo ${SUBJECT_SHORTENED}
     openssl x509 -in $f -out "${CERTIFICATE_STORE_PATH}/${SUBJECT_SHORTENED}/${DATE_NOTBEFORE}_${DATE_NOTAFTER}_${FINGERPRINT}.pem"
     rm $f
   done
